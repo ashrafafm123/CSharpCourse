63bfde9e1cc487679574121f7b6db1b61813a291
Ashraf.Mohammed <Ashraf.Mohammed@QL_AMOHAMMED>
2017-03-29 10:59:34 +0300

    Add project files.

---
+++ firstCSharpAplication/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace firstCSharpAplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
